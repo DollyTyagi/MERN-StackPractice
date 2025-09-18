1d832e2b25c9318b8bd094cfbc94ce2dff40b3fc
dolly <tyagidolly8@gmail.com>
2025-09-18 21:06:37 +0530

    just

--- Week2-js-practice/promises.js
+++ Week2-js-practice/promises.js
@@ -1,27 +1,29 @@
-// function wait (main) {
-//     setTimeout(main , 3000);
-// }
+function wait (main) {
+    setTimeout(main , 3000);
+}
 
-//  function setTimeoutPromisified(){
-//     return new Promise(wait);
-// }
-
-// function main() {
-//     console.log("Hii I am from main")
-// }
-
-// setTimeoutPromisified().then(main);
-
-
-function promiseCallback(main) {
-    setTimeout( main , 3000);
+ function setTimeoutPromisified(){
+    return new Promise(wait);
 }
 
 function main() {
-    console.log("main is called");
+    console.log("Hii I am from main")
 }
 
-promiseCallback(main);
+setTimeoutPromisified().then(main);
+
+// ***Another way***
+
+
+// function promiseCallback(main) {
+//     setTimeout( main , 3000);
+// }
+
+// function main() {
+//     console.log("main is called");
+// }
+
+// promiseCallback(main);
 
 
 
