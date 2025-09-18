5f6a5673954e027d7a5404024b3498439516f69b
dolly <tyagidolly8@gmail.com>
2025-09-18 21:03:34 +0530

    Day5 started with promises

---
+++ Week2-js-practice/promises.js
@@ -0,0 +1,29 @@
+// function wait (main) {
+//     setTimeout(main , 3000);
+// }
+
+//  function setTimeoutPromisified(){
+//     return new Promise(wait);
+// }
+
+// function main() {
+//     console.log("Hii I am from main")
+// }
+
+// setTimeoutPromisified().then(main);
+
+
+function promiseCallback(main) {
+    setTimeout( main , 3000);
+}
+
+function main() {
+    console.log("main is called");
+}
+
+promiseCallback(main);
+
+
+
+
+     
