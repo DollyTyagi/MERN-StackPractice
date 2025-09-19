943ce0a50f4ce0309574da2d5ba0265c9b36511d
dolly <tyagidolly8@gmail.com>
2025-09-20 00:02:23 +0530

    tried promise chaining

--- Week2-js-practice/Assignment.js
+++ Week2-js-practice/Assignment.js
@@ -2,13 +2,49 @@
 // logs hello  3 sec after step 1
 // logs hello  5 sec there after step 2
 
+// setTimeout(function () {
+//     console.log("step 1 print..");
+//     setTimeout(function () {
+//         console.log("step 2 print");
+//         setTimeout (function () {
+//             console.log("step 3 print");
+//         } , 5000);
+//     } , 3000);
+// } , 1000);
 
-setTimeout(function () {
-    console.log("step 1 print..");
-    setTimeout(function () {
-        console.log("step 2 print");
-        setTimeout (function () {
-            console.log("step 3 print");
-        } , 5000);
-    } , 3000);
-} , 1000);
+ // *** promisified version ***
+
+//  setTimeoutPromisified(1000).then ( function() { 
+//     console.log("step 1");
+//     setTimeoutPromisified(3000).then( function () {
+//         console.log("step 2");
+//         setTimeoutPromisified(5000).then(function () {
+//             console.log("step 3");
+//         })
+//     })
+//  })
+
+//  function setTimeoutPromisified(duration) {
+//     return new Promise ( function (resolve) {
+//         setTimeout(resolve, duration);
+//     })
+//  }
+
+//   *** Promise Chaining ***
+
+setTimeoutPromisified(1000).then(function (){
+    console.log("Step 1");
+    return setTimeoutPromisified(3000)
+}).then(function () {
+    console.log("step 2");
+    return setTimeoutPromisified(5000)
+}).then(function () {
+    console.log("step 3");
+})
+
+
+function setTimeoutPromisified(duration) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve ,duration)
+    })
+}
